4f1eb47c30eba0799645b5d0d4b79264296b4d02
gkaragoz <gkaragoz96@gmail.com>
2018-11-19 13:56:44 +0300

    Create MistakeRay.cs script and Mistake.unity scene.

---
+++ Raycast/Assets/Scripts/MistakeRay.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MistakeRay : MonoBehaviour {
+
+    public float rayDistance;
+    public Transform targetObject;
+
+    private void Update() {
+        Debug.DrawRay(transform.position, transform.forward * rayDistance, Color.red);
+
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 1)) {
+            targetObject = hit.transform;
+        } else {
+            targetObject = null;
+        }
+    }
+
+}
