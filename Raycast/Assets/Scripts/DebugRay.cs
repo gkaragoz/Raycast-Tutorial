ffdef79a2d3e483901b80a2b85b08cccc22dccc6
gkaragoz <gkaragoz96@gmail.com>
2018-11-19 13:44:48 +0300

    Create PlayerController.cs script and inital scene.

---
+++ Raycast/Assets/Scripts/DebugRay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugRay : MonoBehaviour {
+
+    public GameObject targetEnemy;
+
+    private void Update() {
+        SetTargetEnemy();
+    }
+
+    private void SetTargetEnemy() {
+        Debug.DrawRay(gameObject.transform.position, transform.forward * 50, Color.green);
+
+        RaycastHit objectHit;
+        // Shoot raycast
+        if (Physics.Raycast(transform.position, transform.forward, out objectHit, 50)) {
+            targetEnemy = objectHit.collider.gameObject;
+        } else {
+            targetEnemy = null;
+        }
+    }
+}
---
+++ Raycast/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float movementSpeed = 2f;
+    public float rotationSpeed = 60f;
+
+    public float shootDistance = 2f;
+
+    public Transform target;
+
+    private void Update() {
+        Vector3 axises = GetInputs();
+
+        if (axises != Vector3.zero) {
+            Move(axises);
+        }
+
+        Rotate();
+
+        AnalyseTarget();
+    }
+
+    private Vector3 GetInputs() {
+        return new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+    }
+
+    public void Move(Vector3 direction) {
+        transform.Translate(direction * movementSpeed * Time.deltaTime);
+    }
+
+    public void Rotate() {
+        if (Input.GetKey(KeyCode.Q)) {
+            transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+        } else if (Input.GetKey(KeyCode.E)) {
+            transform.Rotate(Vector3.down * rotationSpeed * Time.deltaTime);
+        }
+    }
+
+    public void AnalyseTarget() {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+
+        Debug.DrawRay(transform.position, transform.forward * shootDistance, Color.red);
+
+        if (Physics.Raycast(ray, out hit, shootDistance)) {
+            target = hit.transform;
+        } else {
+            target = null;
+        }
+    }
+
+}
